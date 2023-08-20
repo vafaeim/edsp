8e463d78e6c16bafb2b7c8f065d36332db9cc2bf
Amirreza Vafaei Moghadam <vafaeim@icloud.com>
2023-08-21 00:51:51 +0330

    Update GetInformations.java

--- edsp/GetInformations.java
+++ edsp/GetInformations.java
@@ -6,42 +6,44 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class GetInformations {
-	
-	private File file;
-	private BufferedReader read;
-	
-	String address;
-	
-	public GetInformations(String file_address) throws IOException {
-		this.address = file_address;
-		file = new File(address);
-		read = new BufferedReader(new FileReader(file));
-	}
-	
-	public int NumberOfLines() throws IOException {
-		int n = 0;
-		String line = "Empty";
-		while (line != null) {
-			line = read.readLine();
-			n += 1;
-		}
-		read.close();
-		return n;
-	}
-	
-	public long SizeByte() {
-		return file.length();
-	}
-	
-	public Double SizeKB() {
-		return (double) (file.length() / 1000);
-	}
-	
-	public Double SizeMB() {
-		return (double) file.length() / 1000000;
-	}
-	
-	public long SizeGB() {
-		return file.length() / 1000000000;
-	}
+    
+    private File file;
+    private BufferedReader read;
+    
+    String address;
+    
+    public GetInformations(String file_address) throws IOException {
+        this.address = file_address;
+        file = new File(address);
+        read = new BufferedReader(new FileReader(file));
+    }
+    
+    public int NumberOfLines() throws IOException {
+        int n = 0;
+        try {
+            String line;
+            while ((line = read.readLine()) != null) {
+                n++;
+            }
+        } finally {
+            read.close();
+        }
+        return n;
+    }
+    
+    public long SizeByte() {
+        return file.length();
+    }
+    
+    public long SizeKB() {
+        return file.length() / 1024;
+    }
+    
+    public long SizeMB() {
+        return file.length() / (1024 * 1024);
+    }
+    
+    public long SizeGB() {
+        return file.length() / (1024 * 1024 * 1024);
+    }
 }
